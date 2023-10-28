e91ac8cc33bc5c8463bcb44105e77600d70e93b8
Sasha Keller <vanilleschlecker@gmail.com>
2023-10-29 01:50:37 +0200

    complete longest common prefix leetcode problem

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -8,10 +8,43 @@ import {Component} from '@angular/core'
 export class PlaygroundComponent {
 
   constructor() {
-    this.kthGrammar(30, 434991989)
+    this.longestPalindrome('babad')
   }
 
-  kthGrammar(n: number, k: number): number {
-    return (n == 1) ? 0 : (n == 2) ? (k == 1) ? 0 : 1 : (this.kthGrammar(n-1 , (k % 2 == 0) ? k/2 : (k+1)/2) == 0) ? (k % 2 == 0) ? 1 : 0 : (k % 2 == 0) ? 0 : 1
+  longestPalindrome(s: string): string {
+    let sArray = s.split('')
+    let testChar = ''
+    let longest: string[] = []
+    let i = 0
+    let j = 0
+
+    while (i < sArray.length) {
+      if(longest.length > sArray.length / 2) {
+        break
+      }
+      j = i + 1
+      testChar = sArray[i]
+      while (j < sArray.length) {
+        if (testChar[testChar.length - 1] == sArray[j]) {
+          if(sArray.slice(i, j + 1).join('') == sArray.slice(i, j + 1).reverse().join('')) {
+            if (sArray.slice(i, j + 1).length > longest.length) {
+              longest = sArray.slice(i, j + 1)
+            }
+          }
+        }
+        j++
+      }
+      i++
+    }
+
+    if (longest.length > 0) {
+      return longest.join('')
+    } else {
+      if (sArray.length == 0) {
+        return ''
+      } else {
+        return sArray[0]
+      }
+    }
   }
 }
